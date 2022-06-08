d7e76d649fa4b545993b1f19c50256742cc41ae6
Andrés Zamora <zencsoft@gmail.com>
2022-06-08 09:13:00 -0500

    Meta new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,55 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<html lang = "{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta charset = "utf-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1">
+        <meta name = "csrf-token" content = "{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <title>{{ config("app.name", "Laravel") }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <meta http-equiv = "Content-Security-Policy" content = "upgrade-insecure-requests">
 
-        <!-- Styles -->
+        <link href = "https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel = "stylesheet">
+
+        @vite(["resources/css/app.css", "resources/js/app.js"])
+
         @livewireStyles
+
     </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <body class = "font-sans antialiased">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <x-jet-banner/>
+
+        <div class = "min-h-screen bg-gray-100">
+
+            @livewire("navigation-menu")
+
+            @if(isset($header))
+
+                <header class = "bg-white shadow">
+                    <div class = "max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
+
             @endif
 
-            <!-- Page Content -->
             <main>
+
                 {{ $slot }}
+
             </main>
+
         </div>
 
-        @stack('modals')
+        @stack("modals")
 
         @livewireScripts
+
     </body>
+
 </html>
